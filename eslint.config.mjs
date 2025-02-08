89048f88b88c2d20a75fa1b21778679016934f3f
Akhila Ariyachandra <akhila_ariyachandra@live.com>
2025-02-08 10:24:09 +0530

    Extended ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// @ts-check
+import eslintReact from "@eslint-react/eslint-plugin";
 import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/require-await": "off",
+    },
+  },
+  compat.config({
+    extends: ["next/core-web-vitals"],
+  }),
+  compat.config({
+    plugins: ["react-compiler"],
+    rules: {
+      "react-compiler/react-compiler": "error",
+    },
+  }),
+  {
+    extends: [eslintReact.configs["recommended-type-checked"]],
+  },
+  eslintConfigPrettier,
+);
